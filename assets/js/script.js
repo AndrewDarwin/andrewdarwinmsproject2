cc3b13abc2dc0328dd599c586a48a6e23ef73f05
AndrewDarwin <djdarz@gmail.com>
2021-06-29 12:13:44 +0000

    tidying up script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,11 @@
 // I used https://henriqueperoni.github.io/CI-MS2-Magic-Island/ Things To Do Section to assist me in creating the code below -*/
 
-skip.addEventListener("click", () => {
-  for (let i = 0; i < intro.length; i++) {
-    intro[i].style.display = "none";
-  }
-  skip.style.display = "none";
-  introAnim.play("nav, .hero-text");
-});
-
-
 const navSlide = () => {
-  const thingstodo = document.querySelector(".thingstodo");
+  const thingstodo = document.querySelector("section#thingstodo");
   const nav = document.querySelector(".nav-links");
   const navClose = document.querySelectorAll(".nav-anchor");
 
 // I used https://www.codecademy.com/forum_questions/5093e0a0cdbd510200000548 to assist me in creating the code below -*/
-
-  // Open any of Things To Do options
-  thingstodo.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-  });
 
   // Close the option by clicking any of the navigation bar links
   for (i = 0; i < navClose.length; i++) {
