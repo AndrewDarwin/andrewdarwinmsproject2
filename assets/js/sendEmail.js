e6b3282ab4540f263000ab88c05a50ddacedc782
AndrewDarwin <djdarz@gmail.com>
2021-06-29 14:35:01 +0000

    made a text change for the sent message when clicked submit button on contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -9,7 +9,7 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            document.getElementById('feedback-email').innerHTML = 'Thank you for your contact, you should get an email soon.'
+            document.getElementById('feedback-email').innerHTML = 'Thank you for your message, you will receive a reply soon.'
             contactForm.reset();
         },
         function(error) {
